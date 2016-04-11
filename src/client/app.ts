4820dcd8dda56efeb17b6286cb95058471922f23
Jek Bao CHOO <jerchoo@outlook.com>
2016-04-11 20:46:59 +0800

    Initial commit

--- src/client/app.ts
+++ src/client/app.ts
@@ -1,4 +1,10 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AngularFire2LearnApr2016App} from './app/angular-fire2-learn-apr2016';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire} from 'angularfire2';
 
-bootstrap(AngularFire2LearnApr2016App, []);
+bootstrap(AngularFire2LearnApr2016App, [
+    FIREBASE_PROVIDERS,
+    defaultFirebase('https://angularfire2learnapr.firebaseio.com'),
+    ROUTER_PROVIDERS
+]);
--- src/client/app/angular-fire2-learn-apr2016.ts
+++ src/client/app/angular-fire2-learn-apr2016.ts
@@ -1,22 +1,28 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {CliRouteConfig} from './route-config';
+import {AngularFire} from 'angularfire2';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
-  selector: 'angular-fire2-learn-apr2016-app',
-  providers: [ROUTER_PROVIDERS],
-  templateUrl: 'app/angular-fire2-learn-apr2016.html',
-  directives: [ROUTER_DIRECTIVES],
-  pipes: []
+    selector: 'angular-fire2-learn-apr2016-app',
+    providers: [ROUTER_PROVIDERS],
+    templateUrl: 'app/angular-fire2-learn-apr2016.html',
+    directives: [ROUTER_DIRECTIVES],
+    pipes: []
 })
-@RouteConfig([
-
-].concat(CliRouteConfig))
+@RouteConfig([].concat(CliRouteConfig))
 
 export class AngularFire2LearnApr2016App {
-  defaultMeaning: number = 42;
+    defaultMeaning:number = 42;
 
-  meaningOfLife(meaning?: number) {
-    return `The meaning of life is ${meaning || this.defaultMeaning}`;
-  }
+    meaningOfLife(meaning?:number) {
+        return `The meaning of life is ${meaning || this.defaultMeaning}`;
+    }
+
+    items: Observable<any[]>;
+    constructor(af: AngularFire) {
+        // create a list at /items
+        this.items = af.list('/items');
+    }
 }
